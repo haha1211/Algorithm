9f4d82c5d4532f7a88e5aa75feb5c7354082f2bb
haha1211 <ms990905@gmail.com>
2022-01-26 23:43:27 +0900

    1238

--- BOJ/Dijkstra/1238.py
+++ BOJ/Dijkstra/1238.py
@@ -1,3 +1,5 @@
+# 1238.파티
+
 import sys
 import heapq
 
@@ -9,9 +11,13 @@ back_graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
     u, v, w = map(int, input().split())
+    # u -> v
     graph[u].append((v, w))
+    # v -> u
+    # 단방향 도로지만, 모든 학생들의 집에서 x로 가는 최소시간을 한 번에 구하기 위해 반대 방향에 대한 정보도 저장
     back_graph[v].append((u, w))
 
+# dijkstra
 def dijkstra(start, graph):
     dist = [float("INF")] * (n+1)
     Q = [(0, start)]
@@ -26,8 +32,11 @@ def dijkstra(start, graph):
 
     return dist
 
+# x -> 각 학생들의 마을 로 가는데 걸리는 최소시간
 result = dijkstra(x, graph)
+# 각 학생들의 마 -> x 로 가는데 걸리는 최소시간
 back_result = dijkstra(x, back_graph)
 
+# x로 오고 가는데 가장 오래 걸리는 학생 구하기
 result = max(result[i] + back_result[i] for i in range(1, n+1))
 print(result)
