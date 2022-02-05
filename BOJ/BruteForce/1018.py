43bb8b357a60ca50eb35561a2ec0e55c4e4ff380
haha1211 <ms990905@gmail.com>
2022-02-05 23:58:16 +0900

    1018

--- BOJ/BruteForce/1018.py
+++ BOJ/BruteForce/1018.py
@@ -1,3 +1,5 @@
+# 1018.체스판 다시 칠하기
+
 import sys
 
 input = sys.stdin.readline
@@ -11,6 +13,7 @@ for _ in range(n):
 def check(start, end, chess, color):
     change= 0
     key = -1
+    # 시작(0, 0)이 'W'일 경우 다시 칠해야 하는 정사각형의 최소 개수 구하기
     for i in range(start, start+8):
         key *= -1
         for j in range(end, end+8):
@@ -18,10 +21,13 @@ def check(start, end, chess, color):
                 change += 1
             key *= -1
     
+    # 시작이 'B'일 경우는 'W'일 경우와 정반대, 즉 64(전체 개수)에서 빼주면 됨
     return min(change, 64-change)
 
+# 체스판 크기 : 8 * 8
 change = 8 * 8
 
+# 주어진 보드에서 체스판 만들 수 있는 모든 경우 확인
 for i in range(n-8, -1, -1):
     for j in range(m-8, -1, -1):
         change = min(change, check(i, j, chess, color))
