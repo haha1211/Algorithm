f6e769e6f886be760397c300bf7c0cb60fd94e05
haha1211 <ms990905@gmail.com>
2022-02-07 23:46:24 +0900

    1065

--- BOJ/BruteForce/1065.py
+++ BOJ/BruteForce/1065.py
@@ -1,12 +1,18 @@
+# 1065.한수
+# 어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면 그 수를 한수라 한다
+
 import sys
 
 input = sys.stdin.readline
 
+# 한수인지를 확인하는 함수
 def check_ap(n):
     nums = []
+    # 자릿수 찾기
     while n > 0:
         nums.append(n % 10)
         n //= 10
+    # 100(두 자리)이하의 수는 항상 한수
     if len(nums) < 3:
         return True
     else:
@@ -21,10 +27,8 @@ if n < 100:
     total = n
 else:
     total = 99
-    check_ap(157)
     for i in range(100, n+1):
         if check_ap(i):
             total += 1
 
-        
 print(total)
