7c3d1ca8dcc4874395e1d19187404c12235ffdb2
haha1211 <ms990905@gmail.com>
2022-02-09 01:16:50 +0900

    4673

--- BOJ/BruteForce/4673.py
+++ BOJ/BruteForce/4673.py
@@ -1,5 +1,7 @@
-import numbers
+# 4673.셀프 넘버
+# 셀프 넘버 : 생성자가 없는 숫자
 
+import numbers
 
 nums = [False] * (10001)
 
@@ -13,10 +15,14 @@ def sum(num):
 
 
 for i in range(10000):
+    # i의 분해합 num을 구한 후 num을 True로
+    # 생성자가 존재함을 표시
     num = i + sum(i)
+    # 범위 내에서만 위 과정 진행
     if num <= 10000:
         nums[num] = True
 
+# 생성자가 없는 셀프 넘버들 출력
 for i in range(10001):
     if nums[i] == False:
         print(i)
