b23a9b18a413c323c1c6d07d3f49b1db95a1c63a
haha1211 <ms990905@gmail.com>
2022-02-09 01:08:25 +0900

    2231

--- BOJ/BruteForce/2231.py
+++ BOJ/BruteForce/2231.py
@@ -1,3 +1,5 @@
+# 2231.분해합
+
 n = input()
 
 length = len(n)
@@ -12,6 +14,10 @@ def sum(num):
     return total
 
 result = 0
+# X(3) = K + k1+k2+k3 일때
+# X(3) - (k1+k2+k3) = K이고 k1+k2+k3의 최댓값이 3*9이므로
+# 그 미만의 숫자는 생성자가 될 수 X
+# X - (9 * X의 길이) ~ X까지만 탐색하면 됨
 for i in range(max(1,x-(length*9)), x):
     if i + sum(i) == x:
         result = i
