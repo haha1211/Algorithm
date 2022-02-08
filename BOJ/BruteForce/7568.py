1a0197f8f5c7ed174569ee8fe56d0f2c63f307d5
haha1211 <ms990905@gmail.com>
2022-02-09 01:19:57 +0900

    7568

--- BOJ/BruteForce/7568.py
+++ BOJ/BruteForce/7568.py
@@ -1,9 +1,12 @@
+# 7568.덩치
+
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
 body = []
+# 몸무게 정보, 번호 저장
 for i in range(n):
     x, y = map(int, input().split())
     body.append((x,y,i))
@@ -13,9 +16,12 @@ rank = [0] * n
 
 for i in range(n):
     large = 1
+    # 각 사람 별로 그 사람보다 덩치가 큰 사람들 찾기
+    # 등수는 자신보다 덩치큰 사람 +1이므로 1부터 시작
     for j in range(n):
         if body[i][0] < body[j][0] and body[i][1] < body[j][1]:
             large += 1
+    # 처음 저장했던 번호에 맞춰서 등수 저장
     rank[body[i][2]] = large
 
 print(*rank)
