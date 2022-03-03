a620df11fd962256552f977efb211f9b38b63ec6
haha1211 <ms990905@gmail.com>
2022-03-03 11:03:11 +0900

    14430

--- BOJ/DynamicProgramming/14430.py
+++ BOJ/DynamicProgramming/14430.py
@@ -5,19 +5,24 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+# 자원 정보
 graph = []
-result = []
+dp = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-    result.append([0] * m)
+    dp.append([0] * m)
 
-result[0][0] = graph[0][0]
+# Dynamic Programming
+dp[0][0] = graph[0][0]
 
 for i in range(n):
     for j in range(m):
+        # 오른쪽 확인
         if j+1 < m :
-            result[i][j+1] = max(result[i][j+1], result[i][j] + graph[i][j+1])
+            dp[i][j+1] = max(dp[i][j+1], dp[i][j] + graph[i][j+1])
+        # 아래 확인
         if i+1 < n :
-            result[i+1][j] = max(result[i+1][j], result[i][j]+graph[i+1][j])
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j]+graph[i+1][j])
 
-print(result[n-1][m-1])
+# 탐색할 수 있는 자원의 최대 숫자
+print(dp[n-1][m-1])
